28b5ae5b20e3ce7656230f499c441347304741d4
GO <95852069+GoLaboRat@users.noreply.github.com>
2022-10-23 00:51:56 +0900

    Add files via upload

---
+++ CPUバージョン/3.中精度（Small CPU版）.bat
@@ -0,0 +1,38 @@
+@echo off
+
+@rem 実行ディレクトリを取得
+@set your_directory=%~dp0
+
+@rem 入力ファイルの取得
+@set content=%*
+@call :GET_FILE_INFO %content%
+@:GET_FILE_INFO
+@set name="%~n1"
+
+echo -----------------------------------------
+@rem フォルダ作成
+echo フォルダを作成します！
+
+md %your_directory%\%name%
+echo -----------------------------------------
+
+
+@rem anaconda 実行
+echo anaconda/miniconda実行！
+call conda activate whisper
+echo -----------------------------------------
+
+@rem whisper実行
+
+echo whisperを実行します！！
+echo -----------------------------------------
+echo Smallモデル(中精度CPU 非常に遅い)
+
+whisper %content% --output_dir %your_directory%\%name% --model small --device cpu
+
+echo -----------------------------------------
+echo 作成完了！フォルダに入ってます！
+echo -----------------------------------------
+
+pause
+exit
